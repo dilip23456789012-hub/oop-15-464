b1f79540cded14559e2cbce4509b33be59a655c2
dilip23456789012-hub <dilip23456789012@gmail.com>
2026-09-21 11:52:37 +0530

    add file

---
+++ lab4/lab4_files.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> numbers = {10, 20, 30, 40, 50};
+
+    cout << "Elements of the collection:" << endl;
+
+    for (auto element : numbers)
+    {
+        cout << element << " ";
+    }
+
+    return 0;
+}
